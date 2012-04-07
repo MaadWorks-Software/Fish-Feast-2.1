ed3d388a08a5ddd7dfd6959b80079e37ae2d2d9d
Devon Weaver <devon.weaver@gmail.com>
2012-04-06 23:07:23 -0400

    new object class for all objects to be inherited from

---
+++ BaseObject.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Drawing;
+
+
+namespace FishFeast
+{
+	public class BaseObject
+	{
+		
+		public Point Pos;
+		
+		public int Speed {
+			get;
+			set;
+		}
+		
+		public BaseObject ()
+		{
+		}
+	}
+}
+
